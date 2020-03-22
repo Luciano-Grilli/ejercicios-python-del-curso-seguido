4cb00f5f004164fdac10d72c65a029e89b030eaa
Luciano-Grilli <lucianogrilli03@gmail.com>
2020-03-22 13:57:24 -0300

    episodio 6

---
+++ funciones II ep6.py
@@ -0,0 +1,15 @@
+mensaje="hola mundo"
+#procedimiento
+#parametros pueden ser variables o directamente el valor
+def impMensaje(mp):
+    print(mp)
+
+impMensaje("appppp")
+impMensaje(mensaje)
+
+#funcion
+#enviar parametros y devuelve el valor con return
+def suma(num1,num2):
+    return num1+num2
+
+print(suma(5,6))    
---
+++ funsiones I ep5.py
@@ -0,0 +1,9 @@
+def mensaje():
+    print("hola1")
+    print("hola2")
+    print("hola3")
+    print("")
+
+mensaje()
+mensaje()
+mensaje()
